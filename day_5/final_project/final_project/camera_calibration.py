11a79bbcd663475c8bfcd99ded9fb43ad0415db9
summerer512 <109203682+summerery7@users.noreply.github.com>
2026-07-20 09:03:28 +0900

    Delete day_5 directory

--- day_5/final_project/final_project/camera_calibration.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("camera_calibration")
-
-
-if __name__ == "__main__":
-    main()
--- day_5/final_project/final_project/yolo_detector.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("yolo_detector")
-
-
-if __name__ == "__main__":
-    main()
